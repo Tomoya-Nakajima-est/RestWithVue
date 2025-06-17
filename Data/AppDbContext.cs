048d3a25abb7b88ce70fd646cb5f40932dbd784b
EAST/中島朋弥 <t-nakajima@est.co.jp>
2025-06-17 12:03:09 +0900

    12 clock

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RESTwithVue.Models;
+
+namespace RESTwithVue.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options){ }
+        public DbSet<Player> Players { get; set; }
+        
+    }
+}
---
+++ Data/SeedData_Player.cs
@@ -0,0 +1,21 @@
+﻿using RESTwithVue.Models;
+
+namespace RESTwithVue.Data
+{
+    public class SeedData_Player
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if(!context.Players.Any())
+            {
+                context.Players.Add(new Player
+                {
+                    Id = 1,
+                    Name = "Player",
+                    Number = 50
+                });
+                context.SaveChanges();
+            }
+        }
+    }
+}
